36db8a5b0049d327fe63ff87d92af6a5cff13546
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-08 15:06:34 -0300

    feat: create game card component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
+import logoImg from "../../assets/logo-nlw-esports.png";
 
+import { GAMES } from "../../utils/games";
 import { styles } from "./styles";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard data={GAMES[0]} />
+    </View>
+  );
 }
