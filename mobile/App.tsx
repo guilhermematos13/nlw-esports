c4c9012bc151225612a106c290d18f86a3b40c0c
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-08 12:57:23 -0300

    feat:  created loading component, home and install fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,31 @@
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 import { Background } from "./src/components/Background";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_600SemiBold,
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
     <Background>
-      <View></View>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
