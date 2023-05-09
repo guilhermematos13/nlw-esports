6e3f8e7f4597750cfd76c551ee705a0894ba5fdb
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-09 14:36:37 -0300

    feat: connect with api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,29 @@ import './styles/main.css';
 import logoImg from '../src/assets/logo-esports.svg';
 import { GameBanner } from './components/GameBanner';
 import { CreatAdBanner } from './components/CreatAdBanner';
+import { useEffect, useState } from 'react';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -17,11 +38,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={5}
-        />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
       </div>
 
       <CreatAdBanner />
