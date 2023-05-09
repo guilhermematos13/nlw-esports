dc797ec6b755757009a2f3dfaf75a46f5c36490e
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-05-09 20:52:08 -0300

    feat: created game card component

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,44 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import { styles } from './styles';
+import { DuoInfo } from '../DuoInfo';
+import { THEME } from '../../theme';
+import { GameController } from 'phosphor-react-native';
+
+export interface DuoCardProps {
+  id: string;
+  hourEnd: string;
+  hourStart: string;
+  name: string;
+  userVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+}
+
+export function DuoCard({ data }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo label="Nome" value={data.name} />
+      <DuoInfo label="Tempo de jogo" value={`${data.yearsPlaying} anos`} />
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart[0]}${data.hourStart[1]}h - ${data.hourEnd[0]}${data.hourEnd[1]}h`}
+      />
+      <DuoInfo
+        label="Chamada de áudio?"
+        value={data.userVoiceChannel ? 'Sim' : 'Não'}
+        colorValue={
+          data.userVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT
+        }
+      />
+      <TouchableOpacity style={styles.button}>
+        <GameController color={THEME.COLORS.TEXT} size={20} />
+        <Text style={styles.buttonTitle}>Conectar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 import { THEME } from '../../theme';
 import { styles } from './styles';
@@ -10,10 +10,12 @@ import { GameParams } from '../../@types/navigation';
 
 import { Background } from '../../components/Background';
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
 import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
@@ -21,6 +23,12 @@ export function Game() {
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.0.201:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  }, []);
 
   return (
     <Background>
@@ -45,6 +53,12 @@ export function Game() {
         />
 
         <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <DuoCard data={item} />}
+        />
       </SafeAreaView>
     </Background>
   );
